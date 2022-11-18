58c5c37603e8848b7a913ed0cdeaaa1e9fa89435
drewilgenfritz1 <ailgenfritz@vmware.com>
2022-11-18 18:36:00 +0000

    output test

---
+++ terraform/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  value = aws_vpc.TKGVPC.id
+}
+
+output "priv_subnet_a" {
+  value = aws_subnet.priv-a.id
+}
+
+output "priv_subnet_b" {
+  value = aws_subnet.priv-b.id
+}
+
+output "priv_subnet_c" {
+  value = aws_subnet.priv-c.id
+}
+
+output "pub_subnet_a" {
+  value = aws_subnet.pub-a.id
+}
+
+output "pub_subnet_b" {
+  value = aws_subnet.pub-b.id
+}
+
+output "pub_subnet_c" {
+  value = aws_subnet.pub-c.id
+}
+
+output "region"{
+    value = var.aws_region
+}
+
+output "subnetcidrs" {
+    value = [
+      aws_subnet.pub-c.id,
+      aws_subnet.pub-c.cidr_block
+
+    ]
+}
